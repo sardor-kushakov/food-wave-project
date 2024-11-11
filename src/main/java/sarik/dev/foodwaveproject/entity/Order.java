a4e160c7fb0c855509d0d7257da50e69fc7b0bf9
Azizbek <murtozayevazizbek09@gmail.com>
2024-11-11 17:12:37 +0500

    commit2 from Azizbek

--- src/main/java/sarik/dev/foodwaveproject/entity/Order.java
+++ src/main/java/sarik/dev/foodwaveproject/entity/Order.java
@@ -1,12 +1,14 @@
 package sarik.dev.foodwaveproject.entity;
 
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapKeyJoinColumn;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -23,21 +25,38 @@ import java.util.Map;
 @NoArgsConstructor
 @Entity
 public class Order {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private Long userId;
+
+    @Column(nullable = false)
     private LocalDateTime createdAt;
+
+    @Column(nullable = false)
     private LocalDateTime deliveredAt;
+
+    @Column(nullable = false)
     private Status status;
+
     @ElementCollection
-    @MapKeyJoinColumn(name = "food_id")
-    @Column(name = "products")
-    private Map<Product , Integer> products;
+    @CollectionTable(name = "order_products", joinColumns = @JoinColumn(name = "order_id"))
+    @MapKeyJoinColumn(name = "product_id") // Map kaliti sifatida `Product` ishlatiladi
+    private Map<Product, Integer> products;
+
+    @Column(nullable = false)
     private String location;
+
+    @Column(nullable = false)
     private String userPhone;
+
+    @Column(nullable = false)
     private String userEmail;
+
     private String userMessage;
+
+    @Column(nullable = false)
     private Long price;
 }
