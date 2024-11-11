64bc5ad4cdccf16a8fffd8bf2c68fefa0f5184c0
Azizbek <murtozayevazizbek09@gmail.com>
2024-11-11 17:13:05 +0500

    commit2 from Azizbek

---
+++ src/main/java/sarik/dev/foodwaveproject/Auditable.java
@@ -0,0 +1,29 @@
+package sarik.dev.foodwaveproject;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.CurrentTimestamp;
+import org.hibernate.annotations.SourceType;
+import org.hibernate.generator.EventType;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Auditable {
+    @CurrentTimestamp(event = EventType.INSERT, source = SourceType.VM)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    private Long createdBy;
+
+    @CurrentTimestamp(event = EventType.UPDATE, source = SourceType.VM)
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    private Long updatedBy;
+}
