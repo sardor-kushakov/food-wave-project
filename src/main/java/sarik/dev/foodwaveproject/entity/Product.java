3bc1e243aae73c771aa1e0aad733f6f55e4be3b1
Azizbek <murtozayevazizbek09@gmail.com>
2024-11-12 19:36:07 +0500

    Base crud for product from Azizbek

--- src/main/java/sarik/dev/foodwaveproject/entity/Product.java
+++ src/main/java/sarik/dev/foodwaveproject/entity/Product.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,35 +22,34 @@ import java.util.List;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @NotBlank
-//    @Size(min = 3, message = "Product name must contain atleast 3 characters")
+    @NotBlank
+    @Size(min = 3, message = "Product name must contain atleast 3 characters")
     private String productName;
 
-
+    @Positive
     private double price;
 
     private String image;
 
-//    @NotBlank
-//    @Size(min = 6, message = "Product description must contain atleast 6 characters")
+    @NotBlank
+    @Size(min = 6, message = "Product description must contain atleast 6 characters")
     private String description;
 
-//    @NotEmpty
-@ElementCollection
-private List<String> ingredients = new ArrayList<>();
+    @NotEmpty
+    @ElementCollection
+    private List<String> ingredients = new ArrayList<>();
 
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
 
     ///////////////////////
     private Integer quantity;
     private double discount;
     private double specialPrice;
-
-    @ManyToOne
-    @JoinColumn(name = "category_id")
-    private Category category;
 
 //    @OneToMany(mappedBy = "product", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
 //    private List<CartItem> products = new ArrayList<>();
