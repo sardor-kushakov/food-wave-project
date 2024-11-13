1deb1de798347dd97cf6cfa41b4ae869542e87df
bekk-01 <oybekolimov001@gmail.com>
2024-11-13 17:08:54 +0500

    security full from  Oybek

---
+++ src/main/java/sarik/dev/foodwaveproject/configuration/CustomUserDetails.java
@@ -0,0 +1,55 @@
+package sarik.dev.foodwaveproject.configuration;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import sarik.dev.foodwaveproject.entity.auth.AuthRole;
+import sarik.dev.foodwaveproject.entity.auth.AuthUser;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private transient final AuthUser authUser;
+
+    public CustomUserDetails(AuthUser authUser) {
+        this.authUser = authUser;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<AuthRole> roles = authUser.getRoles();
+
+        if(roles == null){
+            return Collections.emptyList();
+        }
+        Set<GrantedAuthority> authorities = new HashSet<>();
+
+        for (AuthRole role : roles) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_"+role.getRoleName()));
+        }
+        return authorities;
+    }
+    public AuthUser getAuthUser() {
+        return authUser;
+    }
+
+    @Override
+    public String getPassword() {
+        return authUser.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return authUser.getEmail();
+    }
+}
---
+++ src/main/java/sarik/dev/foodwaveproject/repository/AuthUserRepository.java
@@ -0,0 +1,13 @@
+package sarik.dev.foodwaveproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sarik.dev.foodwaveproject.entity.auth.AuthUser;
+
+import java.util.Optional;
+
+
+public interface AuthUserRepository extends JpaRepository<AuthUser,Long> {
+
+    Optional<AuthUser> findByEmail(String email);
+}
