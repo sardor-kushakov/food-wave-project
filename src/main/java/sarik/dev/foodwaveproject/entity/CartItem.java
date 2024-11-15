3b5cf9707a4f0fd438af92b21432701144657d76
IppoDev <berdiqolovmuzaffar@gmail.com>
2024-11-15 13:55:05 +0500

    cart added 4 commit from Elmurodov

--- src/main/java/sarik/dev/foodwaveproject/entity/CartItem.java
+++ src/main/java/sarik/dev/foodwaveproject/entity/CartItem.java
@@ -1,9 +1,6 @@
 package sarik.dev.foodwaveproject.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,21 +12,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CartItem {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long cartItemId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartItemId;
 
-	@ManyToOne
-	@JoinColumn(name = "cart_id")
-	private Cart cart;
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
 
-	@ManyToOne
-	@JoinColumn(name = "product_id")
-	private Product product;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
 
 
-	private Integer quantity;
-	private long discount;
-	private long productPrice;
+    private Integer quantity;
+    private long discount;
+    private long productPrice;
 
 }
--- src/main/java/sarik/dev/foodwaveproject/entity/Product.java
+++ src/main/java/sarik/dev/foodwaveproject/entity/Product.java
@@ -1,22 +1,10 @@
 package sarik.dev.foodwaveproject.entity;
-import jakarta.persistence.*;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
-
-
-
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
