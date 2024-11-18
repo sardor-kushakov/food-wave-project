88bef1f16cfce931853cd16e0c9e71f1ab1a4e81
bekk-01 <oybekolimov001@gmail.com>
2024-11-18 16:49:48 +0500

    update test

--- src/main/java/sarik/dev/foodwaveproject/configuration/SecurityConfiguration.java
+++ src/main/java/sarik/dev/foodwaveproject/configuration/SecurityConfiguration.java
@@ -38,6 +38,9 @@ public class SecurityConfiguration {
     private final OTPAuthenticationProvider otpAuthenticationProvider;
 
     public static final String[] WHITE_LIST = {
+            "/api/auth/verify-otp",
+            "/api/auth/register",
+            "/api/auth/login",
             "/swagger-ui/**",
             "/swagger-resources/**",
             "/v3/api-docs/**",
